d32d50d9ee302c553e90858b0729e3b5f58598a7
Hikaru <hikaru@nexaorion.tech>
2025-05-22 22:22:34 +0800

    add suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
+      <body className="flex flex-col min-h-screen" suppressHydrationWarning>
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
